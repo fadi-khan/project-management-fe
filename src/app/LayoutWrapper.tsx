dad3c3ff04cbb922d2d56804760d970a8b735e2c
fadi-khan <fk336733@gmail.com>
2026-02-04 18:35:15 +0500

    fix build error

--- src/app/LayoutWrapper.tsx
+++ src/app/LayoutWrapper.tsx
@@ -3,12 +3,16 @@ import { MainHeader } from "@/components/headers/MainHeader";
 import { ToastProvider } from "@/components/toasts/ToastProvider";
 import QueryProvider from "@/lib/Providers/QueryClientProvider";
 import ReduxProvider from "@/lib/Providers/ReduxProvider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MobileSideBar } from "@/components/sidebars/MobileSidebar";
 
 export const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
     return (
         <QueryProvider>
             <ReduxProvider>
@@ -17,7 +21,7 @@ export const LayoutWrapper = ({ children }: { children: React.ReactNode }) => {
                 <div className="lg:flex">
                     <MobileSideBar isOpen={isOpen} setIsOpen={() => setIsOpen(!isOpen)} />
                     <div className="min-w-0 flex-1">
-                        {children}
+                        {isMounted && children}
                     </div>
                 </div>
             </ReduxProvider>
