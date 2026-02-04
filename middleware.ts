420ac6fb6e6c481b1690e760677792c27894a42a
fadi-khan <fk336733@gmail.com>
2026-02-05 00:51:45 +0500

    middleware experimenting 7

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+import { httpService } from '@/lib/services/HttpService'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
@@ -9,36 +10,23 @@ export const config = {
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
-  const cookie = request.cookies.get('access_token')?.value
-
-
-  console.log('cookie', cookie)
   const isPublicRoute = PUBLIC_ROUTES.some(
     (route) => pathname === route || pathname.startsWith(`${route}/`)
   )
 
+  if (isPublicRoute) {
+    return NextResponse.next()
+  }
 
-    try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/profile`, {
-        method: 'GET',
-        headers: {
-          cookie: `access_token=${cookie}`,
-        },
-        cache: 'no-store',
-      })
 
-      if (!res.ok) throw new Error('Not authenticated')
-      const user = await res.json()
 
-      if (user && isPublicRoute) {
-        return NextResponse.redirect(new URL('/dashboard', request.url))
-      }
+  try {
+    const res = await httpService.get("auth/profile")
 
-      return NextResponse.next()
-    } catch (err) {
-      return NextResponse.redirect(new URL('/login', request.url))
-    }
-  
+    if (!res) throw new Error('Invalid token')
 
-  return NextResponse.next()
+    return NextResponse.next()
+  } catch {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
 }
