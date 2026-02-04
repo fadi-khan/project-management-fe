f4a5936b1c6bb7d66bb72369ec56158da8ee39a3
fadi-khan <fk336733@gmail.com>
2026-02-05 00:15:51 +0500

    experimentig with middleware2

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,47 @@
-import { RootState } from '@/lib/store/store'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { useSelector } from 'react-redux'
 
 const PUBLIC_ROUTES = ['/login', '/signup']
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|api).*)'],
 }
-    const { isAuthenticated } = useSelector((state: RootState) => state.auth)
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
+  const cookie = request.cookies.get('access_token')?.value
 
 
+  console.log('cookie', cookie)
   const isPublicRoute = PUBLIC_ROUTES.some(
-    (route) => pathname === route || pathname.startsWith(`${route}/`),
+    (route) => pathname === route || pathname.startsWith(`${route}/`)
   )
 
-  if (!isAuthenticated && !isPublicRoute) {
+  if (!cookie && !isPublicRoute) {
     return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  if (cookie) {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/profile`, {
+        method: 'GET',
+        headers: {
+          cookie: `access_token=${cookie}`,
+        },
+        cache: 'no-store',
+      })
+
+      if (!res.ok) throw new Error('Not authenticated')
+      const user = await res.json()
+
+      if (user && isPublicRoute) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+      }
+
+      return NextResponse.next()
+    } catch (err) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
   }
 
   return NextResponse.next()
